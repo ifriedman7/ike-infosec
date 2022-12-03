84d6eda61bd3a4a7043342a5c561cff0e089e900
ifriedman7 <97619375+ifriedman7@users.noreply.github.com>
2022-12-03 21:35:32 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -19,7 +19,7 @@ app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
 app.use(helmet.hsts({force: true, maxAge: 7776000000})); // 7776000000ms == 90 days
-
+app.use(helmet.dnsPrefetchControl());
 
 
 
